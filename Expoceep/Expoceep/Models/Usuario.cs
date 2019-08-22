82cee636b9fa4561cde3229613aaf46c37606193
wellhz1n <48489005+wellhz1n@users.noreply.github.com>
2019-08-22 20:44:42 -0300

    ususario

---
+++ Expoceep/Expoceep/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expoceep.Models
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public Usuario(string name)
+        {
+            this.Nome = name;
+        }
+    }
+}
