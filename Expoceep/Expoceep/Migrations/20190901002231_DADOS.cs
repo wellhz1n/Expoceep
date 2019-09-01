344e2f2eb5b346fa198eb153ec995d5dbd5cd87b
Wellington <wellingtom1234520@gmail.com>
2019-08-31 21:23:05 -0300

    DADOS

---
+++ Expoceep/Expoceep/Migrations/20190901002231_DADOS.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Expoceep.Migrations
+{
+    public partial class DADOS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
