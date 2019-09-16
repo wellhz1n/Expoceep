fec593a083ac4f4df930eff5f8ec54e7c0eff6ae
wellhz1n <wellingtom1234520@gmail.com>
2019-09-16 14:34:01 -0300

    Bug Login Fixed

--- Expoceep/Expoceep/wwwroot/js/Login/Login.js
+++ Expoceep/Expoceep/wwwroot/js/Login/Login.js
@@ -3,10 +3,11 @@
     let login = $("#form").serializeArray();
     if (login[0].value !== "" && login[1].value !== "") {
         $(".caixa-login").addClass("anim");
+        BloquearTela();
         $("#form").submit();
     }
     else
-        alert("Preencha os campos");
+        toastr.warning("Preencha os campos", "Login", { timeOut: 2500, progressBar: true, preventDuplicates:true })
 
     //$.post("Login/Login", { login: login[0].value, senha: login[1].value }, function (data) {
     //    if (!data) {
