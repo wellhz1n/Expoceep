c7a3ae678f4874b2ce2ddaaa970d4da7173f3b89
Wellington <wellingtom1234520@gmail.com>
2019-09-17 18:58:11 -0300

    Nao de Update-database

--- Expoceep/Expoceep/DB/ERPDatabaseContext.cs
+++ Expoceep/Expoceep/DB/ERPDatabaseContext.cs
@@ -14,8 +14,8 @@ namespace Expoceep.DB
             Tabelas.Add(typeof(Usuario).Name);
             Tabelas.Add(typeof(Produto).Name);
             Tabelas.Add(typeof(ProdutoPropriedades).Name);
-            Tabelas.Add(typeof(Venda).Name);
-            Tabelas.Add(typeof(Cliente).Name);
+            //Tabelas.Add(typeof(Venda).Name);
+            //Tabelas.Add(typeof(Cliente).Name);
             GeraAdmin();
 
         }
@@ -37,8 +37,8 @@ namespace Expoceep.DB
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<ProdutoPropriedades> ProdutosPropriedadess { get; set; }
-        public DbSet<Venda> Vendas { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
+        //public DbSet<Venda> Vendas { get; set; }
+        //public DbSet<Cliente> Clientes { get; set; }
 
         public List<string> Tabelas = new List<string>();
 
